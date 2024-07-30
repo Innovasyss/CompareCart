817ee8edcfa69fe62e14a8fe52f426734d2377ce
bhaveshwebs <itzzayn1imvu@gmail.com>
2024-07-30 08:06:27 +0530

    Added new index.html

---
+++ script.js
@@ -0,0 +1,17 @@
+const signUpButton=document.getElementById('signUpButton');
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+const signUpForm=document.getElementById('signup');
+
+signUpButton.addEventListener('click',function(){
+    signInForm.style.display="none";
+    signUpForm.style.display="block";
+})
+signInButton.addEventListener('click', function(){
+    signInForm.style.display="block";
+    signUpForm.style.display="none";
+})
+
+function continueWithoutLogin() {
+    window.location.href = 'main.html'; // Change 'main.html' to your main page URL
+}
